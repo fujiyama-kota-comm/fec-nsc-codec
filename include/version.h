a40ff367100e1b6b270a04bc6de3be1758e27181
fujiyama-kota-comm <koanomi29302@icloud.com>
2025-11-13 23:26:08 +0900

    [v0.1.4] バージョン管理方法の修正

--- include/version.h
+++ include/version.h
@@ -3,8 +3,8 @@
 
 #define NSC_VERSION_MAJOR 0
 #define NSC_VERSION_MINOR 1
-#define NSC_VERSION_PATCH 3
+#define NSC_VERSION_PATCH 4
 
-#define NSC_VERSION "0.1.3"
+#define NSC_VERSION "0.1.4"
 
 #endif
