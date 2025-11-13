effaaa88983adda575ffacb4ecbffcbe62b7604b
fujiyama-kota-comm <koanomi29302@icloud.com>
2025-11-13 23:12:42 +0900

    Bump version to 0.1.1

--- include/version.h
+++ include/version.h
@@ -3,8 +3,8 @@
 
 #define NSC_VERSION_MAJOR 0
 #define NSC_VERSION_MINOR 1
-#define NSC_VERSION_PATCH 0
+#define NSC_VERSION_PATCH 1
 
-#define NSC_VERSION "0.1.0"
+#define NSC_VERSION "0.1.1"
 
 #endif
